8f22af1e2d6d7d44e44acf80bfeae82a939ed936
vishaldubey <vkd2695@gmail.com>
2025-12-22 10:45:00 +0530

    chnges

--- updatelogic/settings.py
+++ updatelogic/settings.py
@@ -118,7 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+# STATIC_URL = 'static/'
+
 STATIC_URL = 'static/'
+STATIC_ROOT = "/var/www/project/updatelogic/static/"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = "/var/www/project/updatelogic/media/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- updatelogic/urls.py
+++ updatelogic/urls.py
@@ -21,13 +21,21 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+
+    
     # JWT URLs
     path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
 
