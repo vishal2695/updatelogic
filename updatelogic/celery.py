b5152d684137b93af10e41e0a24be4c322b607e3
Ubuntu <ubuntu@ip-172-31-1-83.ap-south-1.compute.internal>
2025-12-22 09:27:23 +0000

    chnge

--- updatelogic/celery.py
+++ updatelogic/celery.py
@@ -1,21 +1,19 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'updatelogic.settings')
 
 app = Celery('updatelogic')
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
-
-from celery.schedules import crontab
-
 app.conf.beat_schedule = {
-    'add-dummy-every-2-min': {
+    'add-dummy-student-every-5-min': {
         'task': 'api.tasks.add_dummy_student',
-        'schedule': crontab(minute='*/2'),
+        'schedule': crontab(minute='*/5'),
     },
 }
-
